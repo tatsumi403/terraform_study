dd6b7ecb924662188793e6f5ac3f00c90da9679f
tatsumi403 <greenalpina.b5@gmail.com>
2025-06-27 01:03:27 +0900

    プライベートサブネットのルートテーブル作成&関連付け

--- network.tf
+++ network.tf
@@ -80,3 +80,24 @@ resource "aws_nat_gateway" "nat" {
     Environment = var.environment
   }
 }
+
+resource "aws_route_table" "private_route_table" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+
+  tags = {
+    Name        = "${var.environment}-private-route-table-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
