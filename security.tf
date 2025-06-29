50bebcc2e0566b62b33b845c00d8530055fada02
tatsumi403 <greenalpina.b5@gmail.com>
2025-06-30 01:25:12 +0900

    ALBとEC2用のセキュリティグループ追加

--- security.tf
+++ security.tf
@@ -23,3 +23,66 @@ resource "aws_security_group" "my_security_group" {
     Environment = var.environment
   }
 }
+
+resource "aws_security_group" "alb" {
+  name        = "alb_security_group"
+  description = "Allow web traffic to ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Allow access from ALB"
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "ec2_security_group"
+  description = "Allow web traffic from ALB Security Group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Allow access from ALB Security Group to NAT Gateway"
+    Environment = var.environment
+  }
+}
