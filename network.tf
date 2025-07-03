50e2bccafc87b0c73f567960e567201c1e0edf62
tatsumi403 <greenalpina.b5@gmail.com>
2025-07-04 00:06:12 +0900

    add alb

--- network.tf
+++ network.tf
@@ -112,3 +112,32 @@ resource "aws_lb" "main" {
     Environment = var.environment
   }
 }
+
+resource "aws_lb_target_group" "web" {
+  name     = "ec2-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.my_instance[count.index].id
+  port             = 80
+}
+
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
